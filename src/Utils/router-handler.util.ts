e241872d6a0787c9b36c4eae39f1ae37eef282ed
abdelrahman hassan <abdelrahman.7hassan@gmail.com>
2025-12-16 23:31:27 +0200

    added rate limiting

--- src/Utils/router-handler.util.ts
+++ src/Utils/router-handler.util.ts
@@ -1,9 +1,23 @@
 import { Application, Request, Response, NextFunction } from "express";
 import { productController } from "../Modules";
 import { globalErrorHandler } from "../Middlewares";
+import { rateLimit } from "express-rate-limit";
+
+/* rate Limiter */
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+  message: {
+    message: "Too many requests, please try again later",
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+});
 
 /* Router Handler */
 export const routerHandler = (app: Application) => {
+  /* apply limiter to all routes */
+  app.use(limiter);
   /* Home Route */
   app.get("/", (req: Request, res: Response, next: NextFunction) => {
     res.status(200).json({ message: "Welcome to Product Mangment App!" });
